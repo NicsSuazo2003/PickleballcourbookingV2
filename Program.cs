d77de0d5a18da3790425e9aae6052119cd7e9c5c
NicsSuazo2003 <nicssuazo@gmail.com>
2026-08-29 00:31:44 +0800

    Skip migration - database already has Client support

--- Program.cs
+++ Program.cs
@@ -68,7 +68,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    // await db.Database.MigrateAsync();
+    
 
 
     DbSeeder.Initialize(db);
